8aa9415f257229491774819a45e5cf58ae78fbe6
DarkPvP558 <46353233+DarkPvP558@users.noreply.github.com>
2019-01-16 02:23:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 
 const adminprefix = "+";
-const devs = ['344487452200796160'];
+const devs = ['476188949291991060'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
