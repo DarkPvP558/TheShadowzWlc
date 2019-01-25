06fadfc80f9c7ac6fffc1008d566172abfe17021
DarkPvP558 <46353233+DarkPvP558@users.noreply.github.com>
2019-01-25 06:57:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,6 +207,23 @@ message.channel.send({embed:embed});
 
 
 
+client.on('message', message => {
+    if (message.content === ('.bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@476188949291991060>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
 
 
 
