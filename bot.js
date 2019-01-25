d49ef9dd328be594802a98c3d1b003047b6fadd7
DarkPvP558 <46353233+DarkPvP558@users.noreply.github.com>
2019-01-25 06:29:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,316 +1,106 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const moment = require("moment")
+const fs = require("fs")
 
-
-const adminprefix = "+";
-const devs = ['476188949291991060'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setg')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'setn')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'seta')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'sett')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-});
-
-
-
-
-
-
-
-
-
-
-client.on('ready', () => {
-  console.log('---------------');
-  console.log('Dragon Bot')
-  console.log('---------------')
-});
-
-
-client.on('message', msg => {
-    if(msg.author.bot) return;
-    
-    if(msg.content === '+sr') {
-      client.guilds.forEach(g => {
-        
-        let l = g.id
-        g.channels.get(g.channels.first().id).createInvite({
-          maxUses: 5,
-          maxAge: 86400
-        }).then(i => msg.channel.send(`
-        **
-        Invite Link : <https://discord.gg/${i.code}>
-        Server : ${g.name} | Id : ${g.id} 
-        Owner ID : ${g.owner.id}
-        **
-        `))
-  
-  
-      })
-    }
-    
-  })  
-
-client.on('guildCreate', guild => {
-  var embed = new Discord.RichEmbed()
-  .setColor(0x5500ff)
-  .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك**`)
-      guild.owner.send(embed)
-});
-
-client.on("message", message => {
- if (message.content === "+invite") {
-  const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setFooter('© Premium Bot:heart: جميع الحقوق محفوظة 2017 لــبوت')
-      .addField('شكرا لك لاستخدامك ل شادوز بوت', `غير متوفر الان`)
-  message.author.send({embed});
-
- }
-});
-
-
-
-client.on("message", message => {
- if (message.content === "+support") {
-  const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setFooter('© Premium Bot:heart: جميع الحقوق محفوظة TheShadowzBot')  
-      .addField('سيرفر الدعم الفني', `  فير متوفر  `)
-  message.author.send({embed});
-
- }
-});
-
-
-
-
-client.on('message', message => {
-     if (message.content === "+bot") {
-     let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .addField("**Servers:**" , client.guilds.size)
-  .addField("**Users:**", client.users.size)
-  .addField("**channels:**", client.channels.size)
-  .setTimestamp()
-message.channel.sendEmbed(embed);
-    }
-});
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find('name', `روم الترحيب`)
+let memberavatar = member.user.avatarURL
+if (!welcomer) return;
+if(welcomer) {
+moment.locale('ar-ly');
+var mrx = member.user;
+let mrxembed = new Discord.RichEmbed()
+.setTitle(mrx.username,`#${mrx.discriminator}`)
+.addField('» Joined Discord ago',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+.addField('» Your Number',`${member.guild.memberCount}`,true)
+.setThumbnail(mrx.avatarURL)
+.setFooter("Zombie Games.")
+welcomer.send({embed:mrxembed});          
+}})
 
 client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | مين الي دخل ناو' , `نورت السيرفر يا حب, ${member}`)
-        .addField(':		id: | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                     
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter(`${member.guild.name}`)
-        .setTimestamp()
+member.guild.fetchInvites().then(guildInvites => {
+const ei = invites[member.guild.id];
+invites[member.guild.id] = guildInvites;
+const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+const inviter = client.users.get(invite.inviter.id);
+const logChannel = member.guild.channels.find(channel => channel.name === `main-chat`);
+if(!logChannel) return;
+setTimeout(() => {
+logChannel.send(`Invited By: <@${inviter.id}>`);
+},2000)
+});
+});
+
+client.on('guildMemberAdd',async member => {
+const Canvas = require('canvas');
+const jimp = require('jimp');
+const w = ['./welcome_4.png'];
+let Image = Canvas.Image,
+    canvas = new Canvas(800, 300),
+    ctx = canvas.getContext('2d');
+ctx.patternQuality = 'bilinear';
+ctx.filter = 'bilinear';
+ctx.antialias = 'subpixel';
+ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+ctx.shadowOffsetY = 2;
+ctx.shadowBlur = 2;
+ctx.stroke();
+ctx.beginPath();
+
+fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+    if (err) return console.log(err);
+    let BG = Canvas.Image;
+    let ground = new Image;
+    ground.src = Background;
+    ctx.drawImage(ground, 0, 0, 800, 300);
+
+})
+
+        let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+        jimp.read(url, (err, ava) => {
+            if (err) return console.log(err);
+            ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+         if (err) return console.log(err);
+
+  ctx.font = '36px Arial';
+  ctx.fontSize = '72px';
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "center";
+  ctx.fillText(member.user.username, 545, 177);
+ 
+  ctx.font = '16px Arial Bold';
+  ctx.fontSize = '72px';
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
+ 
+  let Avatar = Canvas.Image;
+  let ava = new Avatar;
+  ava.src = buf;
+  ctx.beginPath();
+  ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(ava, 36, 21, 260, 260);
    
-      channel.sendEmbed(embed);
-    });
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`خرج الحب وسابني لوحدي :( :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(` اصلا عادي بره :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`====شكرا لاستخدامك البوت====`, ' https://d.top4top.net/p_1110dli7g1.png')
-    
-    var channel =member.guild.channels.find('name', 'welcome')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
-	
-client.on('message', message => {
-     if (message.content === "+bot") {
-     let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .addField("**Servers:**" , client.guilds.size)
-  .addField("**Users:**", client.users.size)
-  .addField("**channels:**", client.channels.size)
-  .setTimestamp()
-message.channel.sendEmbed(embed);
-    }
-});	
+  let c = member.guild.channels.find('name', `روم الترحيب`)
+  if(!c) return;
+  c.sendFile(canvas.toBuffer());
 
-
-
-client.on("message", message => {
- if (message.content === "+help") {
-  const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")	
-      .setFooter('======================================================')
-      .setFooter('اوامر الاعضاء')
-      .addField('غير متوفر', `لاضافة البوت الى سيرفرك`)	
-      .addField('+bot' , `لمعرفة البوت`)
-      .addField('+ping' , `لمعرفة سرعة اتصال البوت`)
- 
- 
- 
- message.author.send({embed});
-
-
- }
 });
-	  
-	  
-	  
-client.on('message', message => {
-    if(!message.channel.guild) return;
-if (message.content.startsWith('+ping')) {
-if(!message.channel.guild) return;
-var msg = `${Date.now() - message.createdTimestamp}`
-var api = `${Math.round(client.ping)}`
-if (message.author.bot) return;
-let embed = new Discord.RichEmbed()
-.setAuthor(message.author.username,message.author.avatarURL)
-.setColor('RANDOM')
-.addField('**Time Taken:**',msg + " ms 📶 ")
-.addField('**WebSocket:**',api + " ms 📶 ")
-message.channel.send({embed:embed});
-}
+});
 });
 
+const invites = {};
 
+const wait = require('util').promisify(setTimeout);
 
+client.on('ready', () => {
+wait(1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	  
-client.on('message', async message => {
-            if(!message.channel.guild) return;
-             if (message.content.startsWith("+sets")) {
-let args = message.content.split(' ').slice(1).join(' ');
-            let sigMessage = await args;
-            
-            if (sigMessage === "online") {
-                client.user.setStatus("online");
-                message.author.send("Your status was set to online.");
-            }
-            if (sigMessage === "idle") {
-                client.user.setStatus("idle");
-                message.author.send("Your status was set to idle.");
-            }
-            if (sigMessage === "invisible") {
-                client.user.setStatus("invisible");
-                message.author.send("Your status was set to invisible.");
-            }
-            if (sigMessage === "dnd") {
-                client.user.setStatus("dnd");
-                message.author.send("Your status was set to dnd.");
-            }
-            // message.author.send("." + message.content);
-        
-}
+client.guilds.forEach(g => {
+g.fetchInvites().then(guildInvites => {
+invites[g.id] = guildInvites;
 });
-
-
-client.on('message',async msg => {
-     if(msg.channel.type === "dm") return;
-  if(msg.author.bot) return;
-  var p = "+";
-  if(msg.content.startsWith(p + "sats")) {
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  var ggg= msg.guild.createChannel('SERVER STATS', 'category').then(kk => {
-           var ccc =msg.guild.createChannel('SERVER STATS', 'voice').then(al => {
-                var aa =msg.guild.createChannel('SERVER STATS', 'voice').then(alp => {
-                   var aaa =msg.guild.createChannel('SERVER STATS', 'voice').then(alph => {
-       al.setParent(kk);
-       alp.setParent(kk);
-       alph.setParent(kk);
-       
-     al.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-     alp.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-     alph.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-  setInterval(() => {
-      var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-Seconds = currentTime.getSeconds(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-     al.setName(`Voice Online :[ ${msg.guild.members.filter(m => m.voiceChannel).size} ]`);
-      alp.setName(`Time :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
-        alph.setName(`[ Date : [${Year} - ${Month} - ${Dat} ]`);
- },1000);
-                   })
-    
-                })
-           })
-  })
-           
-  }
- 
 });
-
-
-
-
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
+});
